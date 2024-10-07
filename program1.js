af1065c7ebb0b9dea6cd91f8baa667e4e5be97cb
Pankajverma753 <vermapankaj3616@gmail.com>
2024-10-07 12:53:43 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,8 +1,35 @@
-const getTotalIsles = function (grid) {
+function numIsland (grid){
 
-
-  // write your code here
+if (grid.length ===0) return 0;
+  cont rows =grid.length;
+  cont cols= grid[0].length;
+  let islandCount=0;
+  const dfs=(i,j)=>{
+    if (i<0||j<0||i>=rows||j>=cols||grid[i][j]==='W'){
+      return;
+    }
+    
+    grid[i][j]='W';
+    dfs(i+1,j);
+    dfs(i-1,j);
+    dfs(i,j+1);
+    dfs(i,j-1);
 
 };
+  for (let i=0; i< rows; i++){
+     for (let j=0: j< cols; j++) {
+
+       if(grid[i][j] ==='L'){
+         islandCount++;
+         dfs(i,j);
+       }
+     }
+  } 
+      return islandCount;
+}  
+  
+  
+
+  
 
 module.exports = getTotalIsles;
